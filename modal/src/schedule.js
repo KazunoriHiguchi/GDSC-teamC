6b2ff7db66887e72134f65692fbb4a1fcbe38c50
KazunoriHiguchi <kazunorihiguchi@keio.jp>
2022-05-06 20:14:40 +0900

    header background-img

--- modal/src/schedule.js
+++ modal/src/schedule.js
@@ -1,17 +1,17 @@
-import './schedule.css';
-import ModalApp1 from './modal1';
-import App2 from './App2';
-  
+import './schedule.css'
+import ModalApp1 from './modal1'
+import App2 from './App2'
+
 // Example of a data array that
 // you might receive from an API
 const data = [
-  { name: "1", age: 10},
-  { name: "2", age: 10},
-  { name: "3", age: 10},
-  { name: "4", age: 10},
-  { name: "5", age: 10}
+  { name: '1', age: 10 },
+  { name: '2', age: 10 },
+  { name: '3', age: 10 },
+  { name: '4', age: 10 },
+  { name: '5', age: 10 },
 ]
-  
+
 function App3() {
   return (
     <div className="App">
@@ -31,33 +31,33 @@ function App3() {
             <tr key={key}>
               <td>{val.name}</td>
               <td>
-                <ModalApp1/>
+                <ModalApp1 />
               </td>
               <td>
-                <ModalApp1/>
+                <ModalApp1 />
               </td>
               <td>
-                <ModalApp1/>
+                <ModalApp1 />
               </td>
               <td>
-                <ModalApp1/>
+                <ModalApp1 />
               </td>
               <td>
-                <ModalApp1/>
+                <ModalApp1 />
               </td>
               <td>
-                <ModalApp1/>
+                <ModalApp1 />
               </td>
               <td>
-                <ModalApp1/>
+                <ModalApp1 />
               </td>
             </tr>
           )
         })}
       </table>
-      <App2/>
+      <App2 />
     </div>
-  );
+  )
 }
-  
-export default App3;
+
+export default App3
